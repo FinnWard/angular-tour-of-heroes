1774502c1ec3daf40df5a4b94a4cc27d46e6eb8c
Fionn Mac an Bhaird <fionn.macanbhaird@hotmail.com>
2021-02-28 15:26:01 +0100

    added heroes list included css for heroes component. added hero details page on selection

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HEROES } from '../mock-heros';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  heroes = HEROES;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit()
+  { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
